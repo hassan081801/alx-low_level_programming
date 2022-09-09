d9fd86398cb565d53a0ff6926bc8803f49dbb663
hassan081801 <hassangafar67@gmail.com>
2022-09-08 21:31:09 -0700

    forgot my curly bracket

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Entry point
  * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
- *     */
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
