5ff03d7cc57a12c683d465603ad2681ce4bb7507
hassan081801 <hassangafar67@gmail.com>
2022-09-08 21:39:31 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - Prints sizes of characters
+ *
  * Return: Always 0 (success)
  */
 int main(void)
@@ -11,3 +12,4 @@ int main(void)
 					printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 						printf("Size of a float: %zu byte(s)\n", sizeof(float));
 							return (0);
+}
