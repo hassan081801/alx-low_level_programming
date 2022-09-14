03715588dad53070b69a88798cb521ea3ed14a4c
hassan081801 <hassangafar67@gmail.com>
2022-09-14 05:02:23 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+int print_to_98(int n);
+void print_times_table(int n);
