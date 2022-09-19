4d8a425143105a9e2c4638d2296cfcb17a25585b
hassan081801 <hassangafar67@gmail.com>
2022-09-19 07:28:25 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * 0-reset_to_98.c - take a pointer to an int and update the vale it points to 98.
+ * @n: the pointer to an int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * desc: Header file
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
