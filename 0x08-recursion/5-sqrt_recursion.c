dee390b9b0f975bdf6813af3f4bf3f8b360ee111
hassan081801 <hassangafar67@gmail.com>
2022-10-05 05:25:53 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,44 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ *_evaluate - evaluate function sqrt
+ *@i: interger
+ *@n: interger
+ *Return: evaluate sqrt
+ */
+
+int _evaluate(int i, int n)
+{
+/*Evaluate function*/
+if (n == 0 || n == 1)
+return (n);
+
+else if (i * i < n)
+return (_evaluate(i + 1, n));
+
+else if (i * i == n) /*Condiction base*/
+return (i);
+
+return (-1);
+
+return (-1);
+}
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int r;
+ *_sqrt_recursion - evaluate sqrt
+ *@n: interger
+ *Return: Sgrt_recursion
+*/
 
-    r = _sqrt_recursion(1);
-    printf("%d\n", r);
-    r = _sqrt_recursion(1024);
-    printf("%d\n", r);
-    r = _sqrt_recursion(16);
-    printf("%d\n", r);
-    r = _sqrt_recursion(17);
-    printf("%d\n", r);
-    r = _sqrt_recursion(25);
-    printf("%d\n", r);
-    r = _sqrt_recursion(-1);
-    printf("%d\n", r);
-    return (0);
+int _sqrt_recursion(int n)
+{
+int i = 0;
+
+if (i < 0) /*If n is negative*/
+return (-1);
+
+else
+{
+return (_evaluate(i, n)); /*Recursive call*/
+}
+
 }
